ec7b5ae7c10eaf1b616ea231534cc926ed345d44
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-03-08 19:16:52 +0100

    .

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[type="email"]');
+const textSpace = document.querySelector('textarea[name="message"]');
+const btn = document.querySelector('button[type="submit"]');
+const storageValue = {
+    email: email.value,
+    textSpace: textSpace.value
+};
+
+const change = (e) => {
+    
+    const valueFromStorage = localStorage.getItem("feedback-form-state");
+    const objFromStorage = JSON.parse(valueFromStorage);
+localStorage.setItem("feedback-form-state", JSON.stringify(storageValue))
+ 
+}
+
+form.addEventListener('input', change);
+
 
