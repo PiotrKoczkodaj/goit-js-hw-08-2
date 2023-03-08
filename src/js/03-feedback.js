af01636c1a77db8dcf734f759839c604c4e2e2a3
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-03-08 20:08:38 +0100

    .

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,39 @@
+import throttle from "lodash.throttle";
+
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[type="email"]');
 const textSpace = document.querySelector('textarea[name="message"]');
 const btn = document.querySelector('button[type="submit"]');
-const storageValue = {
-    email: email.value,
-    textSpace: textSpace.value
-};
 
-const change = (e) => {
-    
-    const valueFromStorage = localStorage.getItem("feedback-form-state");
-    const objFromStorage = JSON.parse(valueFromStorage);
-localStorage.setItem("feedback-form-state", JSON.stringify(storageValue))
  
+
+
+form.addEventListener('input', throttle(() => {
+    let inputValues = {
+        email: email.value,
+        textSpace: textSpace.value
+};
+    
+   localStorage.setItem('feedback-form-state', JSON.stringify(inputValues));
+},500))
+
+const storageValue = localStorage.getItem('feedback-form-state');
+
+if (storageValue !== null) {
+
+    email.value = JSON.parse(storageValue).email;
+    textSpace.value = JSON.parse(storageValue).textSpace;
+    
 }
 
-form.addEventListener('input', change);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
+    console.log({
+        email: email.value,
+        textSpace: textSpace.value
+    });
 
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+})
